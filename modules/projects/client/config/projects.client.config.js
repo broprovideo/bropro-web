efbd31ab1703d7c7d0b1804134a9411a336543f4
Carl Thompson <carl@gomomentum.marketing>
2015-05-08 11:00:42 +1200

    Button and copy change
    Create new button and projects to video text change

--- modules/projects/client/config/projects.client.config.js
+++ modules/projects/client/config/projects.client.config.js
@@ -5,20 +5,20 @@ angular.module('projects').run(['Menus',
 	function(Menus) {
 		// Add the projects dropdown item
 		Menus.addMenuItem('topbar', {
-			title: 'Projects',
+			title: 'Videos',
 			state: 'home.projects',
 			type: 'dropdown'
 		});
 
 		// Add the dropdown list item
 		Menus.addSubMenuItem('topbar', 'home.projects', {
-			title: 'List Projects',
+			title: 'My Videos',
 			state: 'home.projects.list'
 		});
 
 		// Add the dropdown create item
 		Menus.addSubMenuItem('topbar', 'home.projects', {
-			title: 'Create Projects',
+			title: 'Create New',
 			state: 'home.projects.create'
 		});
 	}
